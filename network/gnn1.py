9350660c658904fdb9215eb544055b02698350c8
yougoforward <908865817@qq.com>
2019-09-20 22:43:37 +0800

    aaf_com_154

--- network/gnn1.py
+++ network/gnn1.py
@@ -306,7 +306,7 @@ class Part_Graph(nn.Module):
             self.xpp_list_list[self.edge_index[i, 1]].append(self.edge_index[i, 0])
 
         self.decomp_hpu_list = Decomposition(hidden_dim, parts=len(upper_part_list))
-        self.decomp_hpl_list = Decomposition(hidden_dim, parts=len(upper_part_list))
+        self.decomp_hpl_list = Decomposition(hidden_dim, parts=len(lower_part_list))
         self.F_dep_list = Contexture(hidden_dim=hidden_dim, parts=self.cls_p - 1)
         self.part_dp_list = nn.ModuleList([Part_Dependency(hidden_dim) for i in range(self.edge_index_num)])
         self.node_update_list = nn.ModuleList([conv_Update(hidden_dim) for i in range(self.cls_p - 1)])
