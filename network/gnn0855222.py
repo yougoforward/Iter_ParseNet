485697f79855bf9388e5619affc10d524f522e24
yougoforward <908865817@qq.com>
2019-10-11 23:31:19 +0800

    aaf_com_154

--- network/gnn0855222.py
+++ network/gnn0855222.py
@@ -345,13 +345,13 @@ class Part_Graph(nn.Module):
         xp_list_new = []
         for i in range(self.cls_p - 1):
             if i + 1 in self.upper_part_list:
-                message = decomp_pu_list[self.upper_part_list.index(i + 1)] + sum(xpp_list_list[i])
-                #
-                # message = decomp_pu_list[self.upper_part_list.index(i + 1)]
+                # message = decomp_pu_list[self.upper_part_list.index(i + 1)] + sum(xpp_list_list[i])
+
+                message = decomp_pu_list[self.upper_part_list.index(i + 1)]+ torch.max(torch.stack(xpp_list_list[i], dim=1), dim=1, keepdim=False)[0]
             elif i + 1 in self.lower_part_list:
                 message = decomp_pl_list[self.lower_part_list.index(i + 1)] + sum(xpp_list_list[i])
-                #
-                # message = decomp_pl_list[self.lower_part_list.index(i + 1)]
+
+                message = decomp_pl_list[self.lower_part_list.index(i + 1)]+ torch.max(torch.stack(xpp_list_list[i], dim=1), dim=1, keepdim=False)[0]
             xp_list_new.append(self.node_update_list[i](xp_list[i], message))
         return xp_list_new, decomp_pu_att_map, decomp_pl_att_map, Fdep_att_list
 
