57ca23c6758f2413201d4bc9b7f97b1db6362260
yougoforward <908865817@qq.com>
2019-10-12 19:06:52 +0800

    aaf_com_154

--- network/gnn0855212.py
+++ network/gnn0855212.py
@@ -99,8 +99,8 @@ class Dep_Context(nn.Module):
         self.maxpool = nn.AdaptiveMaxPool2d(1)
         self.softmax = nn.Softmax(dim=-1)
 
-        self.project = nn.Sequential(nn.Conv2d(in_dim, hidden_dim, kernel_size=1, padding=0, stride=1, bias=False),
-                                     BatchNorm2d(hidden_dim), nn.ReLU(inplace=False))
+        # self.project = nn.Sequential(nn.Conv2d(in_dim, hidden_dim, kernel_size=1, padding=0, stride=1, bias=False),
+        #                              BatchNorm2d(hidden_dim), nn.ReLU(inplace=False))
         self.img_conv = nn.Sequential(nn.Conv2d(in_dim+hidden_dim, 2*hidden_dim, kernel_size=3, padding=4, stride=1, dilation=4, bias=False),
                                       BatchNorm2d(2*hidden_dim), nn.ReLU(inplace=False),
                                       nn.Conv2d(2*hidden_dim, 2*hidden_dim, kernel_size=3, padding=4, stride=1, dilation=4, bias=False),
