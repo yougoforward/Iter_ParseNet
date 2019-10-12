048af4bc903e5a0e82fe08cd137c348780b1c3f4
yougoforward <908865817@qq.com>
2019-10-12 23:29:00 +0800

    aaf_com_154

--- network/gnn085522.py
+++ network/gnn085522.py
@@ -101,9 +101,9 @@ class Dep_Context(nn.Module):
 
         # self.project = nn.Sequential(nn.Conv2d(in_dim, hidden_dim, kernel_size=1, padding=0, stride=1, bias=False),
         #                              BatchNorm2d(hidden_dim), nn.ReLU(inplace=False))
-        self.img_conv = nn.Sequential(nn.Conv2d(in_dim, 2*hidden_dim, kernel_size=3, padding=4, stride=1, dilation=4, bias=False),
+        self.img_conv = nn.Sequential(nn.Conv2d(in_dim, 2*hidden_dim, kernel_size=3, padding=1, stride=1, dilation=1, bias=False),
                                       BatchNorm2d(2*hidden_dim), nn.ReLU(inplace=False),
-                                      nn.Conv2d(2*hidden_dim, hidden_dim, kernel_size=3, padding=4, stride=1, dilation=4, bias=False),
+                                      nn.Conv2d(2*hidden_dim, hidden_dim, kernel_size=3, padding=1, stride=1, dilation=1, bias=False),
                                       BatchNorm2d(hidden_dim), nn.ReLU(inplace=False),
                                       )
         self.alpha = nn.Parameter(torch.ones(1))
