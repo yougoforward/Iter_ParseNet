1e9cab8b5df00281a958a36db51133605b337cb0
yougoforward <908865817@qq.com>
2019-10-24 22:03:36 +0800

    aaf_com_154

--- train_gnn08514.py
+++ train_gnn08514.py
@@ -40,7 +40,7 @@ def parse_args():
     parser.add_argument('--epochs', default=151, type=int)
     parser.add_argument('--batch-size', default=8, type=int)
     parser.add_argument('--learning-rate', default=7e-3, type=float)
-    parser.add_argument('--lr-mode', type=str, default='poly')
+    parser.add_argument('--lr-mode', type=str, default='cosine')
     parser.add_argument('--ignore-label', type=int, default=255)
     # Checkpoints
     # parser.add_argument('--restore-from', default='./checkpoints/init/resnet152_stem.pth', type=str)
