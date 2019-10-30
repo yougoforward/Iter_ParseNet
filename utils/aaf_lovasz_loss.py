70c0b99de3c42a2138aa15e93be9611bc54a2f9a
yougoforward <908865817@qq.com>
2019-10-30 20:08:45 +0800

    final

--- utils/aaf_lovasz_loss.py
+++ utils/aaf_lovasz_loss.py
@@ -1621,8 +1621,8 @@ class ABRLovaszLoss_List_att(nn.Module):
             pred = F.interpolate(input=preds[0][i], size=(h, w), mode='bilinear', align_corners=True)
             pred = F.softmax(input=pred, dim=1)
             loss.append(lovasz_softmax_flat(*flatten_probas(pred, targets[0], self.ignore_index), only_present=self.only_present))
-        # loss = sum(loss[:-1])/(len(preds[0])-1)+loss[-1]
-        loss = sum(loss)
+        loss = sum(loss[:-1])/(len(preds[0])-1)+loss[-1]
+        # loss = sum(loss)
 
         # half body
         loss_hb = []
@@ -1631,8 +1631,8 @@ class ABRLovaszLoss_List_att(nn.Module):
             pred_hb = F.softmax(input=pred_hb, dim=1)
             loss_hb.append(lovasz_softmax_flat(*flatten_probas(pred_hb, targets[1], self.ignore_index),
                                       only_present=self.only_present))
-        # loss_hb = sum(loss_hb[:-1])/(len(preds[1])-1) + loss_hb[-1]
-        loss_hb = sum(loss_hb)
+        loss_hb = sum(loss_hb[:-1])/(len(preds[1])-1) + loss_hb[-1]
+        # loss_hb = sum(loss_hb)
 
         # full body
         loss_fb = []
@@ -1641,8 +1641,8 @@ class ABRLovaszLoss_List_att(nn.Module):
             pred_fb = F.softmax(input=pred_fb, dim=1)
             loss_fb.append(lovasz_softmax_flat(*flatten_probas(pred_fb, targets[2], self.ignore_index),
                                       only_present=self.only_present))
-        # loss_fb = sum(loss_fb[:-1])/(len(preds[2])-1) + loss_fb[-1]
-        loss_fb = sum(loss_fb)
+        loss_fb = sum(loss_fb[:-1])/(len(preds[2])-1) + loss_fb[-1]
+        # loss_fb = sum(loss_fb)
 
         #decomp fh
         loss_fh_att = []
