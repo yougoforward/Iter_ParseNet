24e678732964383d980374defb7c445d236d774d
yougoforward <908865817@qq.com>
2019-11-01 12:33:23 +0800

    final

--- network/gnn0851.py
+++ network/gnn0851.py
@@ -23,9 +23,14 @@ class Composition(nn.Module):
             BatchNorm2d(hidden_dim), nn.ReLU(inplace=False)
         )
     def forward(self, xh, xp_list, xp_att_list):
-        com_att = sum(xp_att_list)
-        xph_message = sum([self.conv_ch(torch.cat([xh, xp * com_att], dim=1)) for xp in xp_list])
-        return xph_message
+        # com_att = sum(xp_att_list)
+        # xph_message = sum([self.conv_ch(torch.cat([xh, xp * com_att], dim=1)) for xp in xp_list])
+        # return xph_message
+
+        # xp = torch.max(torch.stack(xp_list, dim=1), dim=1, keepdim=False)[0]
+        xp =sum(xp_list)
+        xph = self.conv_ch(torch.cat([xh, xp], dim=1))
+        return xph
 
 
 class Decomposition(nn.Module):
@@ -46,18 +51,32 @@ class Decomposition(nn.Module):
         return decomp_fh_list, decomp_att_list, maps
 
 
+# class Decomp_att(nn.Module):
+#     def __init__(self, hidden_dim=10, parts=2):
+#         super(Decomp_att, self).__init__()
+#         self.conv_fh = nn.Conv2d(hidden_dim, parts+1, kernel_size=1, padding=0, stride=1, bias=True)
+#         self.softmax= nn.Softmax(dim=1)
+#
+#     def forward(self, xf, xh_list):
+#         decomp_map = self.conv_fh(xf)
+#         decomp_att = self.softmax(decomp_map)
+#         decomp_att_list = list(torch.split(decomp_att, 1, dim=1))
+#         return decomp_att_list, decomp_map
 class Decomp_att(nn.Module):
     def __init__(self, hidden_dim=10, parts=2):
         super(Decomp_att, self).__init__()
-        self.conv_fh = nn.Conv2d(hidden_dim, parts+1, kernel_size=1, padding=0, stride=1, bias=True)
+        self.conv_fh = nn.Sequential(
+            nn.Conv2d((parts+1)*hidden_dim, (parts+1)*hidden_dim, kernel_size=1, padding=0, stride=1, bias=False),
+            BatchNorm2d((parts+1)*hidden_dim), nn.ReLU(),
+            nn.Conv2d((parts+1)*hidden_dim, parts+1, groups=parts+1, kernel_size=1, padding=0, stride=1, bias=True),
+            )
         self.softmax= nn.Softmax(dim=1)
 
     def forward(self, xf, xh_list):
-        decomp_map = self.conv_fh(xf)
+        decomp_map = self.conv_fh(torch.cat([xf]+ xh_list, dim=1))
         decomp_att = self.softmax(decomp_map)
         decomp_att_list = list(torch.split(decomp_att, 1, dim=1))
         return decomp_att_list, decomp_map
-
 
 class node_att(nn.Module):
     def __init__(self):
@@ -140,24 +159,39 @@ class Part_Dependency(nn.Module):
         return huv
 
 
+# class conv_Update(nn.Module):
+#     def __init__(self, hidden_dim=10):
+#         super(conv_Update, self).__init__()
+#         self.hidden_dim = hidden_dim
+#         dtype = torch.cuda.FloatTensor
+#         self.update = ConvGRU(input_dim=hidden_dim,
+#                               hidden_dim=hidden_dim,
+#                               kernel_size=(1, 1),
+#                               num_layers=1,
+#                               dtype=dtype,
+#                               batch_first=True,
+#                               bias=True,
+#                               return_all_layers=False)
+#
+#     def forward(self, x, message):
+#         _, out = self.update(message.unsqueeze(1), [x])
+#         return out[0][0]
+
 class conv_Update(nn.Module):
-    def __init__(self, hidden_dim=10):
+    def __init__(self, hidden_dim=10, paths_len=3):
         super(conv_Update, self).__init__()
         self.hidden_dim = hidden_dim
-        dtype = torch.cuda.FloatTensor
-        self.update = ConvGRU(input_dim=hidden_dim,
-                              hidden_dim=hidden_dim,
-                              kernel_size=(1, 1),
-                              num_layers=1,
-                              dtype=dtype,
-                              batch_first=True,
-                              bias=True,
-                              return_all_layers=False)
-
-    def forward(self, x, message):
-        _, out = self.update(message.unsqueeze(1), [x])
-        return out[0][0]
-
+        self.conv_update = nn.Sequential(
+            nn.Conv2d(2 * hidden_dim, 2 * hidden_dim, kernel_size=1, padding=0, stride=1, bias=False),
+            BatchNorm2d(2 * hidden_dim), nn.ReLU(inplace=False),
+            nn.Conv2d(2 * hidden_dim, hidden_dim, kernel_size=1, padding=0, stride=1, bias=False),
+            BatchNorm2d(hidden_dim),
+        )
+        self.relu = nn.ReLU(inplace=False)
+    def forward(self, xp, message):
+        out = self.conv_update(torch.cat([xp, message], dim=1))
+        out = self.relu(xp+out)
+        return out
 
 class DecoderModule(nn.Module):
 
