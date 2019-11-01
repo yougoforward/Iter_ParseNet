a932955c7a6d19eeeae57015cb74c4f31cb3a7c3
yougoforward <908865817@qq.com>
2019-11-02 00:20:57 +0800

    55916

--- network/gnn0851_final_dp3.py
+++ network/gnn0851_final_dp3.py
@@ -147,6 +147,21 @@ class Part_Dependency(nn.Module):
         huv = self.R_dep(torch.cat([F_dep_hu, hv], dim=1))
         return huv
 
+def generate_spatial_batch(featmap_H, featmap_W):
+    import numpy as np
+    spatial_batch_val = np.zeros((1, featmap_H, featmap_W, 8), dtype=np.float32)
+    for h in range(featmap_H):
+        for w in range(featmap_W):
+            xmin = w / featmap_W * 2 - 1
+            xmax = (w + 1) / featmap_W * 2 - 1
+            xctr = (xmin + xmax) / 2
+            ymin = h / featmap_H * 2 - 1
+            ymax = (h + 1) / featmap_H * 2 - 1
+            yctr = (ymin + ymax) / 2
+            spatial_batch_val[:, h, w, :] = \
+                [xmin, ymin, xmax, ymax, xctr, yctr, 1 / featmap_W, 1 / featmap_H]
+    return spatial_batch_val
+    
 class Dep_Context(nn.Module):
     def __init__(self, in_dim=256, hidden_dim=10,):
         super(Dep_Context, self).__init__()
