124dcad4365218582d685988dd4adeb87fd8a7da
yougoforward <908865817@qq.com>
2019-11-07 10:47:55 +0800

    final

--- network/gnn085132.py
+++ network/gnn085132.py
@@ -466,17 +466,19 @@ class GNN_infer(nn.Module):
         # f_seg_final = torch.cat(node_seg_list_final[0:2], dim=1)
         # h_seg_final = torch.cat([node_seg_list_final[0]] + node_seg_list_final[2:4], dim=1)
         # p_seg_final = torch.cat([node_seg_list_final[0]] + node_seg_list_final[4:], dim=1)
-        node_seg = F.interpolate(sum(node_seg)/len(node_seg), (th, tw), mode='bilinear', align_corners=True)
+        # node_seg = F.interpolate(sum(node_seg)/len(node_seg), (th, tw), mode='bilinear', align_corners=True)
+        node_seg = sum(node_seg)/len(node_seg)
         node_seg_list = list(torch.split(node_seg, 1, dim=1))
         f_seg = torch.cat(node_seg_list[0:2], dim=1)
         h_seg = torch.cat([node_seg_list[0]] + node_seg_list[2:4], dim=1)
         p_seg = torch.cat([node_seg_list[0]] + node_seg_list[4:], dim=1)
+        p_seg = F.interpolate(p_seg, (th, tw), mode='bilinear', align_corners=True)
         xphf_infer = node_new
         p_seg_final, h_seg_final, f_seg_final = self.final_cls(xphf_infer, xp, xh, xf, xl)
         # return [p_seg, p_seg_new, p_seg_final], [h_seg, h_seg_new, h_seg_final], [f_seg, f_seg_new, f_seg_final], [decomp_fh_att_map], [decomp_up_att_map], [decomp_lp_att_map]
 
         # return [p_seg, p_seg_new, p_seg_final], [h_seg, h_seg_new, h_seg_final], [f_seg, f_seg_new, f_seg_final], [decomp_fh_att_map], [decomp_up_att_map], [decomp_lp_att_map]
-        return [sum([p_seg, p_seg_final])/2], [sum([h_seg, h_seg_final])/2], [sum([f_seg, f_seg_final])/2], [sum(decomp_fh_att_map)/len(decomp_lp_att_map)], [sum(decomp_up_att_map)/len(decomp_lp_att_map)], [sum(decomp_lp_att_map)/len(decomp_lp_att_map)]
+        return [sum([p_seg, p_seg_final])/2], [h_seg], [f_seg], [sum(decomp_fh_att_map)/len(decomp_lp_att_map)], [sum(decomp_up_att_map)/len(decomp_lp_att_map)], [sum(decomp_lp_att_map)/len(decomp_lp_att_map)]
         # return [p_seg, p_seg_final], [h_seg, h_seg_final], [f_seg, f_seg_final], [sum(decomp_fh_att_map)/len(decomp_lp_att_map)], [sum(decomp_up_att_map)/len(decomp_lp_att_map)], [sum(decomp_lp_att_map)/len(decomp_lp_att_map)]
 
 # class Final_classifer(nn.Module):
@@ -549,8 +551,8 @@ class Final_classifer(nn.Module):
         self.p_cls = nn.Conv2d(in_dim, cls_p, kernel_size=1, padding=0, dilation=1, bias=True)
 
         # self.p_cls = nn.Sequential(nn.Conv2d(in_dim * 3 + (cls_p + cls_h + cls_f - 2) * hidden_dim, cls_p, kernel_size=1, padding=0, stride=1, bias=True))
-        self.h_cls = nn.Sequential(nn.Conv2d(in_dim+(cls_p + cls_h + cls_f - 2) * hidden_dim, cls_h, kernel_size=1, padding=0, stride=1, bias=True))
-        self.f_cls = nn.Sequential(nn.Conv2d(in_dim+(cls_p + cls_h + cls_f - 2) * hidden_dim, cls_f, kernel_size=1, padding=0, stride=1, bias=True))
+        # self.h_cls = nn.Sequential(nn.Conv2d(in_dim+(cls_p + cls_h + cls_f - 2) * hidden_dim, cls_h, kernel_size=1, padding=0, stride=1, bias=True))
+        # self.f_cls = nn.Sequential(nn.Conv2d(in_dim+(cls_p + cls_h + cls_f - 2) * hidden_dim, cls_f, kernel_size=1, padding=0, stride=1, bias=True))
 
     def forward(self, xphf, xp, xh, xf, xl):
         # classifier
@@ -561,10 +563,10 @@ class Final_classifer(nn.Module):
         x_fea = self.relu(self.conv3(x)+xt)
 
         xp_seg = self.p_cls(x_fea)
-        xh_seg = self.h_cls(torch.cat([xphf, xh], dim=1))
-        xf_seg = self.f_cls(torch.cat([xphf, xf], dim=1))
+        # xh_seg = self.h_cls(torch.cat([xphf, xh], dim=1))
+        # xf_seg = self.f_cls(torch.cat([xphf, xf], dim=1))
 
-        return xp_seg, xh_seg, xf_seg
+        return xp_seg
 # class Final_classifer(nn.Module):
 #     def __init__(self, in_dim=256, hidden_dim=20, cls_p=7, cls_h=3, cls_f=2):
 #         super(Final_classifer, self).__init__()
