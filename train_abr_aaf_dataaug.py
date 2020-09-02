08a7f44b5ea6bd5bc6c649f2522dd099f5e93989
yougoforward <908865817@qq.com>
2020-09-02 16:50:56 +0800

    aaf_da

--- train_abr_aaf_dataaug.py
+++ train_abr_aaf_dataaug.py
@@ -15,8 +15,8 @@ from torch.utils import data
 from dataset.combo_dataloader import DataGenerator
 # from dataset.dataloader import DataGenerator
 # from dataset.datasets import DatasetGenerator
-from network.abrnet_se154 import get_model
-# from network.abrnet import get_model
+# from network.abrnet_se154 import get_model
+from network.abrnet import get_model
 from progress.bar import Bar
 # from utils.lovasz_loss import ABRLovaszLoss
 from utils.lovasz_loss import AAF_Loss as ABRLovaszLoss
