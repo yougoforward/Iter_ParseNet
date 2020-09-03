7d2458512e55f7dc124613bc5ac88b003100f566
yougoforward <908865817@qq.com>
2020-09-03 18:30:42 +0800

    aaf_da

--- train_abrnet.py
+++ train_abrnet.py
@@ -17,8 +17,8 @@ from dataset.dataloader import DataGenerator
 from network.abrnet import get_model
 # from network.abrnet import get_model
 from progress.bar import Bar
-# from utils.lovasz_loss import ABRLovaszLoss
-from utils.lovasz_loss import ABRLovaszLoss2 as ABRLovaszLoss
+from utils.lovasz_loss import ABRLovaszLoss
+# from utils.lovasz_loss import ABRLovaszLoss2 as ABRLovaszLoss
 
 from utils.metric import *
 from utils.parallel import DataParallelModel, DataParallelCriterion
